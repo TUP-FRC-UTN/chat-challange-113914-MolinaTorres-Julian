7cd52f0f117ff94783c95107a09a83a238bdc9ad
113914-MolinaTorres-Julian <113914@tecnicatura.frc.utn.edu.ar>
2024-08-30 19:58:36 -0300

    push de mensajes funcionando

--- chat/src/app/components/mensaje-usuario/mensaje-usuario.component.ts
+++ chat/src/app/components/mensaje-usuario/mensaje-usuario.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-mensaje-usuario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './mensaje-usuario.component.html',
   styleUrl: './mensaje-usuario.component.css'
 })
 export class MensajeUsuarioComponent {
+  @Input() nombre: string = "";
+  @Output() mensajeEmitido = new EventEmitter<string>();
+  mensaje: string = "";
 
+  emitirMensaje() {
+    this.mensajeEmitido.emit(this.mensaje);
+    this.mensaje = "";
+  }
 }
--- chat/src/app/components/sala/sala.component.ts
+++ chat/src/app/components/sala/sala.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MensajeUsuarioComponent } from "../mensaje-usuario/mensaje-usuario.component";
 
 @Component({
   selector: 'app-sala',
   standalone: true,
-  imports: [],
+  imports: [MensajeUsuarioComponent],
   templateUrl: './sala.component.html',
   styleUrl: './sala.component.css'
 })
 export class SalaComponent {
+  mensajes: string[] = [];
+  nombre: string = "";
+
+  recibirMensaje(mensaje: string) {
+    this.mensajes.push(mensaje);
+  }
 
 }
